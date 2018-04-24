c777a6c2913eaff9a0794241e5c138a1afdc39ed
Paulo Ricardo <paulo_tolkien@outlook.com>
2018-04-24 12:26:22 -0300

    CalculoFinalizado

--- Program.cs
+++ Program.cs
@@ -14,6 +14,32 @@ namespace calculocpf
             int resto=0;
             int rs1 = 0;
             int rs2 = 0;
+
+            cpfCliente = "44361666865";
+            cpf9 = cpfCliente.Substring(0,9);
+            for(int i=0; i < cpf9.Length;i++)
+            {
+                rs1 += int.Parse(cpf9[i].ToString())*contagem1;
+                contagem1--;
+            }
+
+            resto = rs1%11;
+            if(resto < 2)
+                cpf10 = cpf9+0;
+            else    
+                cpf10 = cpf9+(11-resto);
+
+            for(int i=0; i < cpf10.Length;i++)
+            {
+                rs2 += int.Parse(cpf10[i].ToString())*contagem2;
+                contagem2--;
+            }
+
+            resto = rs2%11;
+            if(resto < 2)
+                Console.WriteLine(cpf10+0);
+            else    
+                Console.WriteLine(cpf10+(11-resto));    
         }
     }
 }
