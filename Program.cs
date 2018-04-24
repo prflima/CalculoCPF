60d106cbdc1eabfb2531dddfce49fc23ea6b342c
Paulo Ricardo <paulo_tolkien@outlook.com>
2018-04-24 11:24:39 -0300

    Estrutura&Variaveis

--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ namespace calculocpf
     {
         static void Main(string[] args)
         {
-            
+            string cpfCliente;
+            string cpf9;
+            string cpf10;
+            int contagem1=10;
+            int contagem2=11;
+            int resto=0;
+            int rs1 = 0;
+            int rs2 = 0;
         }
     }
 }
